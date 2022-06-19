28db68cf599e927694f32e6887360fb3cc9d7e3b
shamaunalam <shamaunalam@gmail.com>
2022-06-20 01:55:46 +0530

    made migrations

--- PlantApp/models.py
+++ PlantApp/models.py
@@ -8,3 +8,14 @@ class Inferences(models.Model):
 
     def __str__(self):
         return self.prediction
+
+class Query(models.Model):
+
+    queryId = models.IntegerField(primary_key=True)
+    query   = models.TextField(max_length=1000,blank=False)
+    image   = models.ImageField(upload_to='query_images',default='media/default.jpg')
+
+
+class Answer(models.Model):
+    query = models.ForeignKey(Query,on_delete=models.CASCADE)
+    res   = models.TextField(max_length=2000,blank=False)
